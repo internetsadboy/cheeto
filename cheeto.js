da46cd9c1deb7b86af260eb6e168e08fe5599ae2
ghostsnstuff <jaredhalpert90@gmail.com>
2014-03-24 09:33:28 -0700

    Update cheeto.js

--- cheeto.js
+++ cheeto.js
@@ -1,22 +1,23 @@
 #! /usr/bin/env node
 
 var tlds = require('fs').readFileSync(__dirname+'/tlds.txt', 'utf8').split('\n')
-	, tlds_url = 'http://data.iana.org/TLD/tlds-alpha-by-domain.txt'
-	, parse = require('url').parse
-	, colors = require('colors')
-	, request = require('request')
-	, utils = require('./utils')
-	, fs = require('fs')
+  , tlds_url = 'http://data.iana.org/TLD/tlds-alpha-by-domain.txt'
+  , parse = require('url').parse
+  , colors = require('colors')
+  , request = require('request')
+  , utils = require('./utils')
+  , fs = require('fs')
 
 // command line argument handling
 if(process.argv.length > 2) {
-	var tld = new tld()
-	if(process.argv[2] === 'update') {
-		tld.update()
-	} else if(process.argv[2] === 'list') {
-		tld.list()
-	} else if(process.argv[2].length === 1) {
-		tld.filter(process.argv[2]) 
+  var tld = new tld()
+  if(process.argv[2] === 'update') {
+    tld.update()
+  } else if(process.argv[2] === 'list') {	
+    tld.list()
+  } else if(process.argv[2].length === 1) {
+    tld.filter(process.argv[2]) 
+  }
 }
 
 // tld constructor
@@ -26,44 +27,44 @@ if(process.argv.length > 2) {
 // list logs tlds to console (cssified)
 // filter logs tlds that match the char arg
 function tld() {
-	if(!(this instanceof tld)) return new tld()
-	this.get = function(url) {
-		if(typeof url !== 'object') {
-			return parse(url).host.split('.')[1]	
-		} else {
-			var results = []
-			for(var i = 0; i < url.length; i++) {
-				results[i] = ''
-				if(!parse(url[i]).host) throw new Error('invalid url "'+url[i]+'"')
-				var temp = parse(url[i]).host.split('.')[1]	
-				for(var j in tlds) {
-					if(temp === tlds[j]) results[i] = temp
-				}
-			}
-			return results
-		}
+  if(!(this instanceof tld)) return new tld()
+  this.get = function(url) {
+    if(typeof url !== 'object') {
+      return parse(url).host.split('.')[1]	
+    } else {
+      var results = []
+      for(var i = 0; i < url.length; i++) {
+        results[i] = ''
+	if(!parse(url[i]).host) throw new Error('invalid url "'+url[i]+'"')
+        var temp = parse(url[i]).host.split('.')[1]	
+	for(var j in tlds) {
+	  if(temp === tlds[j]) results[i] = temp
 	}
-	this.isValid = function(url) {
-		if(typeof url !== 'object') {
-			if(parse(url).host) url = parse(url).host.split('.')[1]
-			for(var i in tlds) {
-				if(url === tlds[i]) return true
-			}
-			return false
-		} else {
-			var results = []
-			for(var i = 0; i < url.length; i++) {
-				results[i] = 0
-				var temp 
-				if(parse(url[i]).host) temp = parse(url[i]).host.split('.')[1]	
-				else temp = url[i]
-				for(var j in tlds) {
-					if(temp === tlds[j]) results[i] = 1
-				}
-			}
-		}
-		return results
+      }
+      return results
+    }
+  }
+  this.isValid = function(url) {
+    if(typeof url !== 'object') {
+      if(parse(url).host) url = parse(url).host.split('.')[1]
+      for(var i in tlds) {
+	if(url === tlds[i]) return true
+      }
+      return false
+    } else {
+      var results = []
+      for(var i = 0; i < url.length; i++) {
+	results[i] = 0
+	var temp 
+	if(parse(url[i]).host) temp = parse(url[i]).host.split('.')[1]	
+	else temp = url[i]
+	for(var j in tlds) {
+	  if(temp === tlds[j]) results[i] = 1
 	}
+      }
+    }
+    return results
+  }
 	this.update = function() {
 		request(tlds_url, function(err, res, body) {
 			if(err) throw err
